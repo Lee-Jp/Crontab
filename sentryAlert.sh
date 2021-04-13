53a0782a21fddd2f21779674197104c5a25a2f30
不是酸柠檬 <38690141+Lee-Jp@users.noreply.github.com>
2021-04-13 18:48:17 +0800

    Create sentryAlert.sh

---
+++ sentryAlert.sh
@@ -0,0 +1,1 @@
+curl -d '{"msgtype":"text","text":{"content":"Sentry测试来自bash"}}' -X POST https://oapi.dingtalk.com/robot/send?access_token=93216f34b538458012df546fb86e0d2a3977927a58296eaf8af1f586e1d2f3de  --header "Content-Type: application/json"
